439d7fdec472935462034621aa2f6958818db58f
Maria Bohorquez <a.maria.bohorquez@gmail.com>
2021-07-31 17:04:27 +0200

    bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,14 @@
 class BookingsController < ApplicationController
-  before_action
 
   def new
-    rick_id = params[:rick_id]
-    morty_id = params[:morty_id]
+    character_id = params[:character_id]
+    associated_character_id = params[:associated_character_id]
     customer_id = params[:customer_id]
-    @booking = Booking.new(rick_id, morty_id, customer_id)
+    if user_signed_in? == false
+      redirect_to new_user_session_path, alert: "Buuuuuurp … Wubba Lubba dub-dub you need to sign yourself in ! 🤦‍♂️"
+    else
+      @booking = Booking.new(rick_id, morty_id, customer_id)
+    end
   end
 
   def create
