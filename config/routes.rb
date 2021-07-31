597d7d59cb03abf15264a57703f8cc69de3e88d5
Matthieu de Villele <matthieu.devillele@gmail.com>
2021-07-31 12:27:52 +0200

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'characters#choose' #afficher la page avec les deux liens rick & morty
+  
+  resources :characters, only: [:index, :show] do 
+    resources :bookings, only: [ :create]
+  end
+  
+  resources :bookings, only: [ :show, :update]
+
+
+  # resources :rating, only [:index, :show, :new, :create, :update] do
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
