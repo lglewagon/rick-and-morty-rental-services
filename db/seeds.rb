9a0627379d517bcb8eb425877ccfae6529bf6c4e
Matthieu de Villele <matthieu.devillele@gmail.com>
2021-08-02 20:09:54 +0200

    View modications

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Character.create(name: 'Morty test', description: 'prout', category: 'Morty', photo_url: 'image', availability: true)
-Character.create(name: 'Morty test 0', description: 'prout', category: 'Morty', photo_url: 'image', availability: true)
-Character.create(name: 'Morty test 1', description: 'prout', category: 'Morty', photo_url: 'image', availability: true)
-Character.create(name: 'Morty test 2', description: 'prout', category: 'Morty', photo_url: 'image', availability: false)
+Character.create(name: 'Morty test', description: 'morty_description', category: 'Morty', photo_url: 'image_morty', availability: true)
+Character.create(name: 'Morty test 0', description: 'morty_description', category: 'Morty', photo_url: 'image_morty', availability: true)
+Character.create(name: 'Morty test 1', description: 'morty_description', category: 'Morty', photo_url: 'image_morty', availability: true)
+Character.create(name: 'Morty test 2', description: 'morty_description', category: 'Morty', photo_url: 'image_morty', availability: false)
 
-Character.create(name: 'Rick test', description: 'prout', category: 'Rick', photo_url: '/Users/matthieudevillele/code/MatthdV/fullstack-challenges/05-Rails/05-Rails-MC-with-images/01-Rails-frontend-quiz/rick-and-morty-rental-services/app/profile.jpeg', availability: true)
-Character.create(name: 'Rick test 0', description: 'prout', category: 'Rick', photo_url: 'image', availability: true)
-Character.create(name: 'Rick test 1', description: 'prout', category: 'Rick', photo_url: 'image', availability: true)
-Character.create(name: 'Rick test 2', description: 'prout', category: 'Rick', photo_url: 'image', availability: false)
+Character.create(name: 'Rick test', description: 'rick_description', category: 'Rick', photo_url: 'image_rick', availability: true)
+Character.create(name: 'Rick test 0', description: 'rick_description', category: 'Rick', photo_url: 'image_rick', availability: true)
+Character.create(name: 'Rick test 1', description: 'rick_description', category: 'Rick', photo_url: 'image_rick', availability: true)
+Character.create(name: 'Rick test 2', description: 'rick_description', category: 'Rick', photo_url: 'image_rick', availability: false)
 
